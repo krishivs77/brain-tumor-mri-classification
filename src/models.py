b5f7ad3c93920681463cba1482a0a0cdddba84ca
Krishiv Shah <krishiv3023@gmail.com>
2026-05-23 00:16:54 -0400

    Moved baseline CNN model to source module

---
+++ src/models.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+
+from config import CLASSES
+
+
+class BaselineCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 28 * 28, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, len(CLASSES))
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
